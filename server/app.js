fb46fa3c32374b42bf3c3e7c782507ef5b3f9ef1
Laura Kressin <laurakressin@gmail.com>
2015-09-28 08:11:20 -0500

    working carousel

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.get('/', function(request, response) {
 });
 
 
-var server = app.listen(3000, function(){
+var server = app.listen(process.env.PORT, function(){
     var port = server.address().port;
     console.log('Listening on port: ', port);
 });
